4af54c4d03b2b1db3caaf22fa9d24c74a02d9977
Mucyolouis <mulouis12@gmail.com>
2023-10-05 02:01:04 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -  a C program that prints using puts and followed by a new line.
+ * Return: 0(Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
